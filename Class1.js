9f069045e5ddd1f1e15fe262c5964cdb58409d10
Misrat Rahman <158587527+MisratRahman@users.noreply.github.com>
2024-03-02 01:36:40 +0600

    uploaded first file

---
+++ Class1.js
@@ -0,0 +1,33 @@
+/* 1. Write code in JS: 
+Declare two variables, then assign your first name and last name. Finally, print your full name including one space between both names.*/
+var a= "Misrat" 
+var b= " Rahman"
+ console.log(a+b) 
+
+ /*2. Write code in JS:    Suppose a = 5, b = "60". Use type conversion and print the sum of the two variables.*/
+ var a=5
+  var b="60" 
+  console.log(a+Number(b)) 
+
+/*3. Write code in JS: Num1 = 59, txt1 = "hello JS". Compare both variables and print the result.*/
+var Num1 = 59 
+var txt1 = "hello Js" 
+console.log( Num1==txt1) 
+/*and the result is false*/
+
+/*4. Write code in JS: Num2 = 344, txtNum = "344". Compare the value of both variables along with the data type and print the result.*/
+
+var Num2=344 
+var txtNum= "344" 
+console.log(Num2===txtNum) 
+/*and the result is false*/
+ 
+/*5. Run the below code in JS, write your output:*/
+var oddNum = 75;
+var evenNum = 90;
+var boolValue = oddNum !== evenNum;
+console.log(boolValue);
+
+
+ 
+
